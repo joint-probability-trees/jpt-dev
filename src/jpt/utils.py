adc062b60a99239fe2459934125a00cf90f26af6
Daniel Nyga <daniel.nyga@t-online.de>
2021-05-03 13:51:01 +0200

    Added: entropy(), max_entropy() and rel_entropy() functions.

--- src/jpt/utils.py
+++ src/jpt/utils.py
@@ -1,3 +1,4 @@
+import math
 from functools import reduce
 
 from dnutils import ifnone
@@ -21,3 +22,32 @@ def tojson(obj):
     elif isinstance(obj, dict):
         return {str(k): tojson(v) for k, v in obj.items()}
     return obj
+
+
+def entropy(p):
+    '''Compute the entropy of the multinomial probability distribution ``p``.
+    :param p:   a ``list`` of probabilities or a ``dict``.
+    :return:
+    '''
+    if isinstance(p, dict):
+        p = list(p.values())
+    return abs(-sum([0 if p_i == 0 else math.log(p_i, 2) * p_i for p_i in p]))
+
+
+def max_entropy(n):
+    '''Compute the maximal entropy that a multinomial random variable with ``n`` states can have,
+    i.e. the entropy value assuming a uniform distribution over the values.
+    :param p:
+    :return:
+    '''
+    return entropy([1 / n for _ in range(n)])
+
+
+def rel_entropy(p):
+    '''Compute the entropy of the multinomial probability distribution ``p`` normalized
+    by the maximal entropy that a multinomial distribution of the dimensionality of ``p``
+    can have.
+    :type p: distribution'''
+    if len(p) == 1:
+        return 0
+    return entropy(p) / max_entropy(len(p))
