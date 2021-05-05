d8fa5fd840676c21bca768dd507a85200b7aae11
Daniel Nyga <daniel.nyga@t-online.de>
2021-05-05 11:02:42 +0200

    Added: SYMBOL class for unicode characters.

--- src/jpt/utils.py
+++ src/jpt/utils.py
@@ -4,6 +4,11 @@ from functools import reduce
 from dnutils import ifnone
 
 
+class SYMBOL:
+    LAND = '\u2227'
+    IN = '\u2208'
+
+
 def mapstr(seq, format=None):
     '''Convert the sequence ``seq`` into a list of strings by applying ``str`` to each of its elements.'''
     return [format(e) for e in seq] if callable(format) else [ifnone(format, '%s') % (e,) for e in seq]
