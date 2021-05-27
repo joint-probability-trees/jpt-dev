eba5e6f129d99acd3462c1e3da19c168113e1120
Mareike Picklum <mareikep@cs.uni-bremen.de>
2021-05-27 11:30:39 +0200

    Update: moved examples into their respective files in examples folder, cleanup main.py

--- examples/muesli.py
+++ examples/muesli.py
@@ -1,15 +1,25 @@
+import pyximport
+pyximport.install()
 import itertools
 import os
+import pickle
+from datetime import datetime
 
+import numpy as np
 import pandas as pd
-import pyximport
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-pyximport.install()
+from dnutils import out
+from jpt.base.quantiles import Quantiles
+from jpt.learning.distributions import Numeric, Bool, SymbolicType
+from jpt.trees import JPT
+from jpt.variables import SymbolicVariable, NumericVariable
+from jpt.base.intervals import ContinuousSet as Interval
 
 
 def plot_muesli():
+    # Used to create the plots for the paper
 
     # df = pd.read_pickle('data/human_muesli.dat')
     df = pd.read_csv('data/muesli.csv')
@@ -26,29 +36,103 @@ def plot_muesli():
     icons = itertools.cycle(['o', 'x', '1'])
     colors = ['orange', 'cornflowerblue', 'green', 'yellow']
 
-    # if only original data is used:
+    # whether extra dimension (success) is used:
     succ = False
-    for color, icon, clazz in zip(colors, icons, df['Class'].unique()):
-        samples = df[df['Class'] == clazz]
-        ax.scatter(samples['X'], samples['Y'], color=color, marker=icon, label=clazz)
-
-    # if extra dimension (success) is used:
-    # succ = True
-    # for icon, clazz in zip(icons, df['Class'].unique()):
-    #     samples = df[df['Class'] == clazz]
-    #     for color, succ in zip(colors, df['Success'].unique()):
-    #         spls = samples[samples['Success'] == succ]
-    #         ax.scatter(spls['X'], spls['Y'], color=color, marker=icon, label=f'{clazz} $\u2192$ {succ}')
+    if succ:
+        for icon, clazz in zip(icons, df['Class'].unique()):
+            samples = df[df['Class'] == clazz]
+            for color, succ in zip(colors, df['Success'].unique()):
+                spls = samples[samples['Success'] == succ]
+                ax.scatter(spls['X'], spls['Y'], color=color, marker=icon, label=f'{clazz} $\u2192$ {succ}')
+    else:
+        for color, icon, clazz in zip(colors, icons, df['Class'].unique()):
+            samples = df[df['Class'] == clazz]
+            ax.scatter(samples['X'], samples['Y'], color=color, marker=icon, label=clazz)
 
     ax.legend()
-
     plt.show()
 
     with PdfPages(os.path.join('data', f'muesli-{2 if succ else 1}.pdf'), metadata={'Creator': 'misc',
-                                                                  'Author': 'Picklum & Nyga',
-                                                                  'Title': f'Müsli Example{" Success" if succ else ""}'}) as pdf:
+                                                                                    'Author': 'Picklum & Nyga',
+                                                                                    'Title': f'Müsli Example{" Success" if succ else ""}'}) as pdf:
         pdf.savefig(fig)
 
 
+def test_muesli():
+    # TODO: check! This does not work anymore -> fix or delete
+
+    data = pd.read_csv('../examples/data/muesli.csv')
+    d = np.array(sorted(data['X']), dtype=np.float64)
+
+    quantiles = Quantiles(d, epsilon=.0001)
+    cdf_ = quantiles.cdf()
+    d = Numeric(quantile=quantiles)
+
+    interval = Interval(-2.05, -2.0)
+    p = d.p(interval)
+    out('query', interval, p)
+
+    print(d.cdf.pfmt())
+    d.plot(title='Piecewise Linear CDF of Breakfast Data', fname='BreakfastPiecewise', xlabel='X', view=True, directory=os.path.join('/tmp', f'{datetime.now().strftime("%d.%m.%Y-%H:%M:%S")}-Muesli'))
+
+
+def muesli_tree():
+    # generate Joint Probability Tree from muesli data (use .csv file because it contains the additional Success column)
+    data = pd.read_csv('../examples/data/muesli.csv')
+    ObjectType = SymbolicType('ObjectType', data['Class'].unique())
+
+    x = NumericVariable('X', Numeric)
+    y = NumericVariable('Y', Numeric)
+    o = SymbolicVariable('Object', ObjectType)
+    s = SymbolicVariable('Success', Bool)
+
+    jpt = JPT([x, y, o, s], name="Breakfasttree", min_samples_leaf=5)
+    jpt.learn(columns=data.values.T)
+    jpt.plot(plotvars=[x, y, o], directory=os.path.join('/tmp', f'{datetime.now().strftime("%d.%m.%Y-%H:%M:%S")}-Muesli'))
+
+    for clazz in data['Class'].unique():
+        out(jpt.infer(query={o: clazz}, evidence={x: [.9, None], y: [None, .45]}))
+    print()
+
+    for clazz in data['Class'].unique():
+        for exp in jpt.expectation([x, y], evidence={o: clazz}, confidence_level=.1):
+            out(exp)
+
+    # plotting vars does not really make sense here as all leaf-cdfs of numeric vars are only piecewise linear fcts
+    # --> only for testing
+    # jpt.plot(plotvars=[x, y, o, s])
+
+    # q = {o: ("BowlLarge_Bdvg", "JaNougatBits_UE0O"), x: [.812, .827]}
+    # r = jpt.reverse(q)
+    # out('Query:', q, 'result:', pprint.pformat(r))
+
+
+def picklemuesli():
+    # read in original .pkl file and pickle as proper np.array
+    f = os.path.join('../' 'examples', 'data', 'human_muesli.pkl')
+
+    data = []
+    with open(f, 'rb') as fi:
+        data = np.array(pickle.load(fi))
+
+    transformed = []
+    for c in data.T:
+        try:
+            transformed.append(np.array(c, dtype=float))
+        except:
+            transformed.append(np.array(c))
+
+    with open(f, 'wb+') as fi:
+        pickle.dump(transformed, fi)
+
+
+def main(*args):
+    # plot_muesli()
+    # test_muesli()
+    muesli_tree()
+    # picklemuesli()
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    plot_muesli()
+    main('PyCharm')
---
+++ examples/neems.py
@@ -0,0 +1,67 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+
+from dnutils import out
+from jpt.learning.distributions import SymbolicType, Numeric, Bool
+from jpt.trees import JPT
+from jpt.variables import SymbolicVariable, NumericVariable
+
+
+def neemdata():
+    # learn from dataset from sebastian (fetching milk from fridge)
+
+    # location of NEEM_SetTable_Breakfast.tar.xz
+    ltargz = 'https://seafile.zfn.uni-bremen.de/f/fa5a760d89234cfc83ad/?dl=1'
+    df = pd.read_csv(ltargz, compression='xz', delimiter=';', sep=';', skip_blank_lines=True, header=0,
+                     index_col=False,
+                     names=['id', 'type', 'startTime', 'endTime', 'duration', 'success', 'failure', 'parent', 'next', 'previous', 'object_acted_on', 'object_type', 'bodyPartsUsed', 'arm', 'grasp', 'effort'],
+                     usecols=['type', 'startTime', 'endTime', 'duration', 'success', 'failure', 'object_acted_on', 'bodyPartsUsed', 'arm'],
+                     na_values=['type', 'startTime', 'endTime', 'duration', 'success', 'failure', 'object_acted_on', 'bodyPartsUsed', 'arm', np.inf])
+
+    # set default values for empty, infinity or nan values and remove nan rows
+    df = df[df['endTime'].notna()]  # this not only removes the lines with endTime=NaN, but in particular all lines where each feature is NaN
+    df.replace([np.inf, -np.inf], -1, inplace=True)
+    df['object_acted_on'] = df['object_acted_on'].fillna('DEFAULTOBJECT')
+    df['bodyPartsUsed'] = df['bodyPartsUsed'].fillna('DEFAULTBP')
+    df['arm'] = df['arm'].fillna('DEFAULTARM')
+    df['failure'] = df['failure'].fillna('DEFAULTFAIL')
+    df['startTime'] = df['startTime'].fillna(-1)
+    df['endTime'] = df['endTime'].fillna(-1)
+    df['duration'] = df['duration'].fillna(-1)
+
+    # type declarations
+    tpTYPE = SymbolicType('type', df['type'].unique())
+    failTYPE = SymbolicType('failure', df['failure'].unique())
+    oaoTYPE = SymbolicType('object_acted_on', df['object_acted_on'].unique())
+    bpuTYPE = SymbolicType('bodyPartsUsed', df['bodyPartsUsed'].unique())
+    armTYPE = SymbolicType('arm', df['arm'].unique())
+
+    # variable declarations
+    tp = SymbolicVariable('type', tpTYPE)
+    st = NumericVariable('startTime', Numeric, haze=.1)
+    et = NumericVariable('endTime', Numeric, haze=.1)
+    dur = NumericVariable('duration', Numeric, haze=.1)
+    succ = SymbolicVariable('success', Bool)
+    fail = SymbolicVariable('failure', failTYPE)
+    oao = SymbolicVariable('object_acted_on', oaoTYPE)
+    bpu = SymbolicVariable('bodyPartsUsed', bpuTYPE)
+    arm = SymbolicVariable('arm', armTYPE)
+
+    vars = [tp, st, et, dur, succ, fail, oao, bpu, arm]
+    jpt = JPT(variables=vars, name="NEEMs", min_samples_leaf=500)
+    out(f'Learning sebadata-Tree...')
+    jpt.learn(columns=df.values.T)
+    out(f'Done! Plotting...')
+    jpt.plot(filename=jpt.name, plotvars=vars, directory=os.path.join('/tmp', f'{datetime.now().strftime("%d.%m.%Y-%H:%M:%S")}-NEEMdata'), view=True)
+
+
+def main(*args):
+    neemdata()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main('PyCharm')
