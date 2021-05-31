ce347b4309570f464d7608e6b05f88be21eb20cc
Daniel Nyga <daniel.nyga@t-online.de>
2021-05-31 15:29:25 +0200

    Fixed: MNIST example data set.

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,30 +1,45 @@
+import numpy as np
+from dnutils import out
+from matplotlib import pyplot as plt
+
+from jpt.learning.distributions import Numeric
+from jpt.trees import JPT
+from jpt.variables import NumericVariable
 
 
-def test_mnist():
+def main():
     from sklearn.datasets import load_digits
     mnist = load_digits()
 
-    tree = JPT(...)
-    tree.fit(np.ascontiguousarray(mnist.data, dtype=np.float32),
-             # np.asarray(mnist.target.reshape(-1, 1), dtype=np.float64))
-             np.ascontiguousarray(mnist.data, dtype=np.float64))
+    variables = [NumericVariable('x_%s%s' % (x1+1, x2+2), Numeric) for x1 in range(8) for x2 in range(8)]
+
+    tree = JPT(variables=variables, min_samples_leaf=150)
+
+    tree.learn(rows=mnist.data)
+
     leaves = list(tree.leaves.values())
 
     rows = len(leaves) // 10 + 1
     cols = max(2, len(leaves))
     fig, axes = plt.subplots(rows, cols, figsize=(16, 6))
-    out(axes)
+
     if len(axes.shape) == 1:
         axes = np.array([axes])
-    out(axes)
     # for i in range(20):
     # axes[i // 10, i % 10].imshow(mnist.images[i], cmap='gray')
     # axes[i // 10, i % 10].axis('off')
     # axes[i // 10, i % 10].set_title(f"target: {mnist.target[i]}")
+
     for i, leaf in enumerate(leaves):
-        out(leaf.dist)
-        model = leaf.dist.mean.reshape(8, 8)
+        out(leaf.distributions, leaf.samples)
+        model = np.array([d.expectation() for d in leaf.distributions.values()]).reshape(8, 8)
+        out(model)
         idx = i // 10, i % 10 if len(axes.shape) == 1 else i
         axes[idx].imshow(model, cmap='gray')
+
     plt.tight_layout()
     plt.show()
+
+
+if __name__ =='__main__':
+    main()
