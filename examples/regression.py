468dd5ce5e208c1cdaaf104eba237d5ba3f98ad0
Daniel Nyga <daniel.nyga@t-online.de>
2021-05-31 20:48:09 +0200

    Fixed: regression example.

--- examples/regression.py
+++ examples/regression.py
@@ -32,6 +32,7 @@ def main():
     # Mesh the input space for evaluations of the real function, the prediction and
     # its MSE
     xx = np.atleast_2d(np.linspace(-25, 20, 500)).T
+    # xx = np.atleast_2d(np.linspace(-10, 20, 500)).T
     xx = xx.astype(np.float32)
 
     # Construct the predictive model
@@ -42,17 +43,18 @@ def main():
     jpt.learn(columns=[X.ravel(), y])
 
     # Apply the JPT model
-    my_predictions = [first(jpt.expectation([vary], evidence={varx: x_})) for x_ in xx.ravel()]
+    confidence = .25
+    my_predictions = [first(jpt.expectation([vary], evidence={varx: x_}, confidence_level=confidence)) for x_ in xx.ravel()]
     y_pred_ = [p.result for p in my_predictions]
     y_lower_ = [p.lower for p in my_predictions]
     y_upper_ = [p.upper for p in my_predictions]
 
     # Plot the function, the prediction and the 90% confidence interval based on the MSE
     fig = plt.figure()
-    plt.plot(xx, f(xx), 'g:', label=u'$f(x) = x\,\sin(x)$')
-    plt.plot(X, y, '.', color='gray', markersize=5, label=u'Observations')
-    plt.plot(xx, y_pred_, 'm-', label=u'My Prediction')
-    plt.plot(xx, y_lower_, 'y--')
+    plt.plot(xx, f(xx), 'g:', label=r'$f(x) = x\,\sin(x)$')
+    plt.plot(X, y, '.', color='gray', markersize=5, label='Training data')
+    plt.plot(xx, y_pred_, 'm-', label='JPT Prediction')
+    plt.plot(xx, y_lower_, 'y--', label='%.1f%% Confidence bands' % (confidence * 100))
     plt.plot(xx, y_upper_, 'y--')
 
     plt.xlabel('$x$')
