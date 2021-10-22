97fc13ae7489c8ad30ba470a38c99a64f4ebf69c
Daniel Nyga <daniel.nyga@t-online.de>
2021-10-22 18:26:45 +0200

    Added: cython compiler arguments.

--- src/jpt/base/cutils.pyx
+++ src/jpt/base/cutils.pyx
@@ -1,2 +1,8 @@
-# cython: language-level=3
+# cython: auto_cpdef=True
+# cython: infer_types=True
+# cython: language_level=3
+# cython: cdivision=True
+# cython: wraparound=False
+# cython: boundscheck=False
+# cython: nonehcheck=False
 # ----------------------------------------------------------------------------------------------------------------------
