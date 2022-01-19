2ce112e5ad33ce3d0be6baa01fbbeefb0e08b5cc
Daniel Nyga <nyga@cs.uni-bremen.de>
2022-01-19 11:28:02 +0100

    Fixed: variable types in MNIST example.

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,5 +1,7 @@
 import numpy as np
-from dnutils import out
+from pandas import DataFrame
+
+from dnutils import mapstr, edict
 from matplotlib import pyplot as plt
 
 from jpt.learning.distributions import Numeric, SymbolicType
@@ -11,38 +13,42 @@ def main():
     from sklearn.datasets import load_digits
     mnist = load_digits()
 
-    variables = ([NumericVariable('x_%s%s' % (x1+1, x2+2), Numeric) for x1 in range(8) for x2 in range(8)])
-    # +
-    #              [SymbolicVariable('class', SymbolicType('DigitType', mnist.target))])
+    # Create the names of the numeric variables
+    pixels = ['x_%s%s' % (x1 + 1, x2 + 1) for x1 in range(8) for x2 in range(8)]
 
-    tree = JPT(variables=variables, min_samples_leaf=150)
+    # Create the data frame
+    df = DataFrame.from_dict(edict({'digit': mapstr(mnist.target)}) +
+                             {pixel: list(values) for pixel, values in zip(pixels, mnist.data.T)})
 
-    # tree.learn(columns=np.vstack([mnist.data.T, mnist.target.T]))
-    tree.learn(columns=mnist.data.T)
+    targets = list(sorted(set(mapstr(mnist.target))))
+    DigitType = SymbolicType('DigitType', targets)
+
+    variables = ([SymbolicVariable('digit', domain=DigitType)] +
+                 [NumericVariable(pixel, Numeric) for pixel in pixels])
+
+    tree = JPT(variables=variables, min_samples_leaf=100)
+
+    tree.learn(data=df)
 
     leaves = list(tree.leaves.values())
 
-    rows = len(leaves) // 10 + 1
-    cols = max(2, len(leaves))
-    fig, axes = plt.subplots(rows, cols, figsize=(16, 6))
+    rows = 2
+    cols = 7
+    fig, axes = plt.subplots(rows, cols, figsize=(7, 2))
 
     if len(axes.shape) == 1:
         axes = np.array([axes])
-    # for i in range(20):
-    # axes[i // 10, i % 10].imshow(mnist.images[i], cmap='gray')
-    # axes[i // 10, i % 10].axis('off')
-    # axes[i // 10, i % 10].set_title(f"target: {mnist.target[i]}")
 
     for i, leaf in enumerate(leaves):
-        model = np.array([d.expectation() for d in list(leaf.distributions.values())]).reshape(8, 8)
-        # print(leaf.distributions[tree.varnames['class']]._p)
-        idx = i // 10, i % 10 if len(axes.shape) == 1 else i
+        model = np.array([leaf.distributions[tree.varnames[pixel]].expectation() for pixel in pixels]).reshape(8, 8)
+        idx = (i // 7, i % 7)
         axes[idx].imshow(model, cmap='gray')
+        axes[idx].set_title(leaf.distributions[tree.varnames['digit']].expectation())
 
     plt.tight_layout()
     plt.show()
     tree.plot()
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
