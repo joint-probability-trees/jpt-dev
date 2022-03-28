4166ceba1f41a65a26801e6ec9af10c3c8051b62
Daniel Nyga <daniel.nyga@t-online.de>
2022-03-28 23:42:14 +0200

    Fixed: test cases.

--- test/test_inference.py
+++ test/test_inference.py
@@ -25,25 +25,26 @@ class JointProbabilityTreesMPE(unittest.TestCase):
         self.M = SymbolicVariable('MaryCalls', Bool)
         self.J = SymbolicVariable('JohnCalls', Bool)
 
-        data = []
         f = os.path.join('../' 'examples', 'data', 'alarm.pkl')
         with open(f, 'rb') as fi:
             data = np.array(pickle.load(fi))
 
-        self.jpt = JPT(variables=[self.E, self.B, self.A, self.M, self.J], min_impurity_improvement=0)
+        self.jpt = JPT(variables=[self.E, self.B, self.A, self.M, self.J],
+                       min_impurity_improvement=0)
+
         self.jpt.learn(rows=data)
 
     def test_infer_alarm_given_mary(self):
         q = {self.A: True}
         e = {self.M: True}
         res = self.jpt.infer(q, e)
-        self.assertAlmostEqual(.92352, res.result, places=5)
+        self.assertAlmostEqual(0.950593, res.result, places=5)
 
     def test_infer_alarm(self):
         q = {self.A: True}
         e = {}
         res = self.jpt.infer(q, e)
-        self.assertAlmostEqual(0.00252, res.result, places=5)
+        self.assertAlmostEqual(0.210199, res.result, places=5)
 
 
 if __name__ == '__main__':
