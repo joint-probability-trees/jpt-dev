24118321830fdf1a273f4641321784614077aaa8
tomsch420 <tom_sch@uni-bremen.de>
2022-04-28 11:42:47 +0200

    JPTBase removed and testcases im test_jpt.py updated. Unfortunately test_inference.py throws segfault.

--- test/test_jpt.py
+++ test/test_jpt.py
@@ -2,7 +2,7 @@ import pickle
 from unittest import TestCase
 
 from jpt.trees import JPT
-from jpt.variables import NumericVariable
+from jpt.variables import NumericVariable, VariableMap
 
 
 class JPTTest(TestCase):
@@ -11,20 +11,29 @@ class JPTTest(TestCase):
         with open('resources/gaussian_100.dat', 'rb') as f:
             self.data = pickle.load(f)
 
+    def test_hyperparameter_serialization(self):
+        '''Serialization with complete hyperparameters without training'''
+        x = NumericVariable('X')
+        y = NumericVariable('Y')
+        variable_dependencies = VariableMap([(x, [x, y]), (y, [x])])
+        jpt = JPT(variables=[x, y],
+                  targets=[x, y],
+                  min_samples_leaf=.1,
+                  min_impurity_improvement=0.1,
+                  max_leaves=100,
+                  max_depth=10,
+                  variable_dependencies=variable_dependencies)
+
+        jpt_ = JPT.from_json(jpt.to_json())
+        self.assertEqual(jpt, jpt_)
+
     def test_serialization(self):
-        '''(de)serialization of JPTs'''
+        '''(de)serialization of JPTs with training'''
 
         var = NumericVariable('X')
         jpt = JPT([var], min_samples_leaf=.1)
         jpt.learn(self.data.reshape(-1, 1))
 
         jpt_ = JPT.from_json(jpt.to_json())
-
-        for l1, l2 in zip(jpt.leaves.values(), jpt_.leaves.values()):
-            self.assertEqual(l1, l2)
-
-        for n1, n2 in zip(jpt.innernodes.values(), jpt_.innernodes.values()):
-            self.assertEqual(n1, n2)
-
-        self.assertEqual(jpt, JPT.from_json(jpt.to_json()))
+        self.assertEqual(jpt, jpt_)
 
