d6d74e2f5eb6232ed717596fe1ff8b1f50cecd5d
tomsch420 <tom_sch@uni-bremen.de>
2022-05-16 16:51:46 +0200

    refactored the shared cdfs, yet some inconsistents bugs appear

--- test/test_sequence_modelling.py
+++ test/test_sequence_modelling.py
@@ -2,8 +2,8 @@ import unittest
 import numpy as np
 import jpt.variables
 from jpt.sequential_jpt import SequentialJPT
+import matplotlib.pyplot as plt
 import itertools
-
 
 class UniformSeries:
 
@@ -42,14 +42,43 @@ class SequenceTest(unittest.TestCase):
         self.assertAlmostEqual(l[0], l[1])
 
     def test_infer(self):
-        tree = SequentialJPT(self.variables, min_samples_leaf=500)
+        tree = SequentialJPT(self.variables, min_samples_leaf=1500)
         tree.learn([self.data])
 
         q_0 = {self.variables[0]: [0.95, 1.05]}
         q_1 = {self.variables[0]: [-1.05, -0.95]}
 
-        p = tree.infer(queries=[q_0,q_1,q_0], evidences=[dict(), dict(), dict()])
+        p = tree.infer(queries=[q_0,q_1,q_0, q_1], evidences=[dict(), dict(), dict(),dict()])
+
+        #for leaf_combo, distributions in tree.shared_dimensions_integral.items():
+         #   for variable, distribution in distributions.items():
+          #      print(leaf_combo, distribution.cdf.intervals, distribution.cdf.functions)
+           #     distribution.plot(title=str(leaf_combo))
+            #    plt.show()
+
         self.assertAlmostEqual(p, 0.5, places=2)
+
+    def test_plot(self):
+        tree = SequentialJPT(self.variables, min_samples_leaf=1500)
+        tree.learn([self.data])
+
+        samples = [-1.101, -1.009, -1.0, -0.995, -0.99, -0.5, 0, 0.5, 0.99, 0.995, 1., 1.009, 1.101]
+        samples = list(itertools.product(samples, samples, samples))
+        samples = np.expand_dims(np.array(samples),2)
+
+        l = tree.likelihood(samples)
+        l = l/l.max()
+        l = np.maximum(l, 0.01)
+        import plotly.graph_objects as go
+
+        fig = go.Figure()
+        for sample, l_ in zip(samples, l):
+            fig.add_trace(go.Scatter(x=[0,1,2], y=sample[:,0], opacity=l_))
+        fig.update_layout(title="Likelihood of different sequences", showlegend=False)
+        fig.update_xaxes(title_text='Timestep')
+        fig.update_yaxes(title_text='X')
+        fig.write_html("../../Documents/expanded_template.html")
+
 
 if __name__ == '__main__':
     unittest.main()
