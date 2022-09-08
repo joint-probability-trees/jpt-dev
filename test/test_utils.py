d586cd98730d2b89fef1cb957441e13143986120
Daniel Nyga <daniel.nyga@t-online.de>
2022-09-08 21:46:42 +0200

    Added: chop() function.

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from jpt.base.utils import mapstr
+from jpt.base.utils import mapstr, chop
 
 
 class UtilsTest(TestCase):
@@ -11,3 +11,21 @@ class UtilsTest(TestCase):
         self.assertEqual(['0', '1', '...', '8', '9'], mapstr(l, limit=4))
         self.assertEqual(['0', '9'], mapstr([0, 9], limit=4))
         self.assertEqual(['0', '...'], mapstr([0, 9], limit=1))
+
+    def test_chop(self):
+        truth = [
+            (0, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+            (1, [2, 3, 4, 5, 6, 7, 8, 9]),
+            (2, [3, 4, 5, 6, 7, 8, 9]),
+            (3, [4, 5, 6, 7, 8, 9]),
+            (4, [5, 6, 7, 8, 9]),
+            (5, [6, 7, 8, 9]),
+            (6, [7, 8, 9]),
+            (7, [8, 9]),
+            (8, [9]),
+            (9, [])]
+        result = []
+        for h, t in chop(list(range(10))):
+            result.append((h, list(t)))
+        self.assertEqual(truth, result)
+        self.assertEqual([], list(chop([])))
