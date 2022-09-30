Fixed: probability computation in Numeric._p() method. Refactored: inference test cases to live in test_jpt module. Fixed: test results in restaurant examples.

src/jpt/distributions/univariate.py
@@ -585,14 +585,18 @@ class Numeric(Distribution):
         return self
 
     def _p(self, value: Union[numbers.Number, NumberSet]) -> numbers.Real:
-        if isinstance(value, numbers.Number) and np.isinf(self.pdf.eval(value)):
-            return 0
+        if isinstance(value, numbers.Number):
+            value = ContinuousSet(value, value)
         elif isinstance(value, RealSet):
             return sum(self._p(i) for i in value.intervals)
-        elif value.lower == value.upper and not value.isempty() and np.isinf(self.pdf.eval(value.lower)):
-            return 1
-        return ((self.cdf.eval(value.upper) if value.upper != np.PINF else 1.) -
-                (self.cdf.eval(value.lower) if value.lower != np.NINF else 0.))
+        probspace = self.pdf.gt(0)
+        if probspace.isdisjoint(value):
+            return 0
+        probmass = ((self.cdf.eval(value.upper) if value.upper != np.PINF else 1.) -
+                    (self.cdf.eval(value.lower) if value.lower != np.NINF else 0.))
+        if not probmass:
+            return probspace in value
+        return probmass
 
     def p(self, labels: Union[numbers.Number, NumberSet]) -> numbers.Real:
         if not isinstance(labels, (NumberSet, numbers.Number)):