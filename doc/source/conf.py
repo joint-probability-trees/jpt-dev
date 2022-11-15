b5c8af5cecc7d7344660e8084aeceeb0c2a3b59e
Tom Schierenbeck <tom_sch@uni-bremen.de>
2022-11-15 18:25:56 +0100

    added regression example

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,6 @@ author = 'Daniel Nyga, Mareike Picklum, Tom Schierenbeck'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
