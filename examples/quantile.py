6bc3f7cbd453f77dad0ce0a19719a2686371a75a
Mareike Picklum <mareikep@cs.uni-bremen.de>
2023-11-14 11:28:03 +0100

    Refactor quantile example: use plotly instead of matplotlib

--- examples/quantile.py
+++ examples/quantile.py
@@ -5,6 +5,7 @@ pyximport.install()
 # from jpt.base.quantiles import QuantileDistribution
 
 from jpt.distributions.quantile.cdfreg import CDFRegressor
+import plotly.graph_objects as go
 
 
 # from jpt.base.intervals import ContinuousSet, INC, EXC
@@ -14,7 +15,6 @@ from jpt.distributions.quantile.quantiles import QuantileDistribution
 
 
 import numpy as np
-from matplotlib import pyplot as plt
 
 from jpt.distributions import Gaussian
 
@@ -46,34 +46,69 @@ def test_quantile_merge():
     dist_all = QuantileDistribution(epsilon=EPS)
     dist_all.fit(data.reshape(-1, 1), None, 0)
 
+    mainfig = go.Figure()
+
     # reg = CDFRegressor(eps=.01)
     # reg.fit(qdata(data))
 
     dist1 = QuantileDistribution(epsilon=EPS)
     dist1.fit(g1data.reshape(-1, 1), None, 0)
-    plt.scatter(g1data.ravel(), np.zeros(g1data.shape[0]), label='Raw data')
+    mainfig.add_trace(
+        go.Scatter(
+            x=g1data.ravel(),
+            y=np.zeros(g1data.shape[0]),
+            marker=dict(
+                symbol='circle',
+                color='blue',
+                size=10,
+            ),
+            mode="markers",
+            name='Raw data'
+        )
+    )
 
     # reg.fit(np.array(qdata(gauss1)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
     dist2 = QuantileDistribution(epsilon=EPS)
     dist2.fit(g2data.reshape(-1, 1), None, 0)
-    plt.scatter(g2data.ravel(), np.zeros(g2data.shape[0]), label='Raw data')
+    mainfig.add_trace(
+        go.Scatter(
+            x=g2data.ravel(),
+            y=np.zeros(g2data.shape[0]),
+            marker=dict(
+                symbol='circle',
+                color='green',
+                size=10,
+            ),
+            mode="markers",
+            name='Raw data'
+        )
+    )
 
     # reg.fit(np.array(qdata(gauss2)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
     dist3 = QuantileDistribution(epsilon=EPS)
     dist3.fit(g3data.reshape(-1, 1), None, 0)
-    plt.scatter(g3data.ravel(), np.zeros(g3data.shape[0]), label='Raw data')
+    mainfig.add_trace(
+        go.Scatter(
+            x=g3data.ravel(),
+            y=np.zeros(g3data.shape[0]),
+            marker=dict(
+                symbol='circle',
+                color='red',
+                size=10,
+            ),
+            mode="markers",
+            name='Raw data'
+        )
+    )
 
     # reg.fit(np.array(qdata(gauss3)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
-    dist = QuantileDistribution.merge([dist1, dist2, dist3], [.333, .333, .333])
+    dist = QuantileDistribution.merge([dist1, dist2, dist3], [1/3, 1/3, 1/3])
 
     x = np.linspace(-7, 9, 500)
     cdf1 = dist1.cdf.multi_eval(x)
@@ -83,17 +118,99 @@ def test_quantile_merge():
     cdf = dist_all.cdf.multi_eval(x)
     cdf_merged = dist.cdf.multi_eval(x)
 
-    plt.plot(x, cdf1, label='CDF-1')
-    plt.plot(x, cdf2, label='CDF-2')
-    plt.plot(x, cdf3, label='CDF-3')
-    plt.plot(x, cdf, label='Combined CDF')
-    plt.plot(x, cdf_merged, label='Merged CDF')
-    # plt.plot(x, pdf, label='Percentile-point fct')
-    # ax.plot(x, dist_all.cdf.multi_eval(x), label='All CDF', linestyle='dashed')
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=np.asarray(cdf1),
+            line=dict(
+                color='blue',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='CDF-1'
+        )
+    )
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=np.asarray(cdf2),
+            line=dict(
+                color='green',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='CDF-2'
+        )
+    )
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=np.asarray(cdf3),
+            line=dict(
+                color='red',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='CDF-3'
+        )
+    )
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=np.asarray(cdf),
+            line=dict(
+                color='purple',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='Combined CDF'
+        )
+    )
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=np.asarray(cdf_merged),
+            line=dict(
+                color='orange',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='Merged CDF'
+        )
+    )
 
-    plt.grid()
-    plt.legend()
-    plt.show()
+    mainfig.update_layout(
+        width=1000,
+        height=1000,
+        xaxis=dict(
+            side='bottom',
+        ),
+        yaxis=dict(
+            range=[0, 1]
+        ),
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=.8
+        ),
+    )
+
+    mainfig.show(
+        config=dict(
+            displaylogo=False,
+            toImageButtonOptions=dict(
+                format='svg',  # one of png, svg, jpeg, webp
+                filename="quantile_merge.svg",
+                scale=1
+            )
+        )
+    )
 
 
 def test_quantiles():
@@ -108,41 +225,34 @@ def test_quantiles():
     reg = CDFRegressor(eps=.01)
     reg.fit(qdata(data))
 
+    reg2 = CDFRegressor(eps=.05)
+    reg2.fit(qdata(data))
+
     # print(reg.cdf.pfmt())
 
     points = np.array(reg.support_points)
-
-    fig, ax = plt.subplots()
-    # ax.set_title("Quantiles")
-
+    points2 = np.array(reg2.support_points)
 
     dist_all = QuantileDistribution(epsilon=.1)
     dist_all.fit(data.reshape(-1, 1), None, 0)
 
-
     # dist1 = QuantileDistribution(epsilon=1e-10)
     # dist1.fit(gauss1)
-    # plt.scatter(gauss1, np.zeros(gauss1.shape[0]), label='Raw data')
 
     # reg.fit(np.array(qdata(gauss1)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
     # dist2 = QuantileDistribution(epsilon=1e-10)
     # dist2.fit(gauss2)
-    # plt.scatter(gauss2, np.zeros(gauss2.shape[0]), label='Raw data')
 
     # reg.fit(np.array(qdata(gauss2)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
     # dist3 = QuantileDistribution(epsilon=1e-10)
     # dist3.fit(gauss3)
-    # plt.scatter(gauss3, np.zeros(gauss3.shape[0]), label='Raw data')
 
     # reg.fit(np.array(qdata(gauss3)).T, presort=1)
     # points = np.array(reg.points)
-    # plt.scatter(points[:, 0], points[:, 1])
 
     # dist = QuantileDistribution.merge([dist1, dist2, dist3], [.333, .333, .333])
 
@@ -161,24 +271,111 @@ def test_quantiles():
     # out('PPF(1) =', dist.ppf.eval(1))
     # out('PPF(0) =', dist.ppf.eval(0))
 
-    x = sorted(np.linspace(-4, 9, 300))
-    ax.plot(points[:, 0], points[:, 1], label='PLF of CDF', marker='o', color='orange')
-    ax.scatter(data, np.zeros(data.shape[0]), label='Raw data', marker='x', color='cornflowerblue')
-    ax.scatter(np.sort(data), np.cumsum(np.ones(data.shape[0]) / data.shape[0]), label='Combined data distribution', marker='+', color='black')
-    ax.plot(x, [gauss1.pdf(d) + gauss2.pdf(d) + gauss3.pdf(d) for d in x], label='Mixture of Gaussians', color='green')
-    # plt.plot(x, cdf1, label='CDF-1')
-    # plt.plot(x, cdf2, label='CDF-2')
-    # plt.plot(x, cdf, label='Combined CDF')
-    # plt.plot(x, pdf, label='Percentile-point fct')
-    # ax.plot(x, dist_all.cdf.multi_eval(x), label='All CDF', linestyle='dashed')
+    x = np.linspace(-4, 9, 300)
+    x.sort()
+    cdf = np.array([gauss1.cdf(d)[0]/3 + gauss2.cdf(d)[0]/3 + gauss3.cdf(d)[0]/3 for d in x])
 
-    plt.grid()
-    plt.legend()
-    plt.show()
+    fname = "Quantile-Example"
+    mainfig = go.Figure()
+
+    # scatter x sin(x) function
+    mainfig.add_trace(
+        go.Scatter(
+            x=points[:, 0],
+            y=points[:, 1],
+            line=dict(
+                color='orange',
+                width=2,
+                dash='solid',
+            ),
+            marker=dict(
+                symbol='circle',
+                color='orange',
+                size=10,
+            ),
+            mode="lines+markers",
+            name=r'$\text{PLF of CDF with }\varepsilon = 0.01$'
+        )
+    )
+
+    mainfig.add_trace(
+        go.Scatter(
+            x=points2[:, 0],
+            y=points2[:, 1],
+            line=dict(
+                color='#9300B9',
+                width=2,
+                dash='solid',
+            ),
+            marker=dict(
+                symbol='x',
+                color='#9300B9',
+                size=10,
+            ),
+            mode="lines+markers",
+            name=r'$\text{PLF of CDF with }\varepsilon = 0.05$'
+        )
+    )
+
+    mainfig.add_trace(
+        go.Scatter(
+            x=data,
+            y=np.zeros(data.shape[0]),
+            marker=dict(
+                symbol='x',
+                color='cornflowerblue',
+                size=10,
+            ),
+            mode='markers',
+            name="Raw data",
+        )
+    )
+
+    mainfig.add_trace(
+        go.Scatter(
+            x=x,
+            y=cdf,
+            line=dict(
+                color='green',
+                width=2,
+                dash='solid',
+            ),
+            mode="lines",
+            name='Mixture of Gaussians'
+        )
+    )
+
+    mainfig.update_layout(
+        width=1000,
+        height=1000,
+        xaxis=dict(
+            side='bottom',
+        ),
+        yaxis=dict(
+            range=[0, 1]
+        ),
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=.8
+        ),
+    )
+
+    mainfig.show(
+        config=dict(
+            displaylogo=False,
+            toImageButtonOptions=dict(
+                format='svg',  # one of png, svg, jpeg, webp
+                filename=fname,
+                scale=1
+            )
+        )
+    )
 
 
 def main(*args):
-    # test_quantiles()
+    test_quantiles()
     test_quantile_merge()
 
 
