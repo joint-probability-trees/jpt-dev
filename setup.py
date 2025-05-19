0895cfda3cf0a2e755a0ceee86a083bd9db3982e
David Prueser <david@pruesernet.de>
2025-05-19 12:51:15 +0200

    Try to fix CI/CD

--- setup.py
+++ setup.py
@@ -7,24 +7,24 @@ import numpy
 from Cython.Build import cythonize
 
 
-sys.path.insert(0, 'src')
-
-
-__description__ = '''Joint Probability Trees (short JPTs) are a formalism for learning of and reasoning about joint probability
-distributions, which is tractable for practical applications. JPTs support both symbolic and subsymbolic variables in a single
-hybrid model, and they do not rely on prior knowledge about variable dependencies or families of distributions.
-JPT representations build on tree structures that partition the problem space into relevant subregions that are elicited
-from the training data instead of postulating a rigid dependency model prior to learning. Learning and reasoning scale
-linearly in JPTs, and the tree structure allows white-box reasoning about any posterior probability :math:`P(Q\mid E)`,
-such that interpretable explanations can be provided for any inference result. This documentation introduces the
-code base of the ``pyjpt`` library, which is implemented in Python/Cython, and showcases the practical
-applicability of JPTs in high-dimensional heterogeneous probability spaces, making it
-a promising alternative to classic probabilistic
-
-## Documentation
-The documentation is hosted on readthedocs.org [here](https://joint-probability-trees.readthedocs.io/en/latest/).'''
-
-
+# sys.path.insert(0, 'src')
+#
+#
+# __description__ = '''Joint Probability Trees (short JPTs) are a formalism for learning of and reasoning about joint probability
+# distributions, which is tractable for practical applications. JPTs support both symbolic and subsymbolic variables in a single
+# hybrid model, and they do not rely on prior knowledge about variable dependencies or families of distributions.
+# JPT representations build on tree structures that partition the problem space into relevant subregions that are elicited
+# from the training data instead of postulating a rigid dependency model prior to learning. Learning and reasoning scale
+# linearly in JPTs, and the tree structure allows white-box reasoning about any posterior probability :math:`P(Q\mid E)`,
+# such that interpretable explanations can be provided for any inference result. This documentation introduces the
+# code base of the ``pyjpt`` library, which is implemented in Python/Cython, and showcases the practical
+# applicability of JPTs in high-dimensional heterogeneous probability spaces, making it
+# a promising alternative to classic probabilistic
+#
+# ## Documentation
+# The documentation is hosted on readthedocs.org [here](https://joint-probability-trees.readthedocs.io/en/latest/).'''
+#
+#
 def read_version(fname):
     try:
         return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
@@ -33,17 +33,17 @@ def read_version(fname):
 
 
 __version__ = read_version(os.path.join('src', 'jpt', '.version'))
-
-
-def requirements():
-    with open('requirements.txt', 'r') as f:
-        return [_.strip() for _ in f.readlines() if _.strip()]
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-# Set up the C++ extensions for compilation
-
-
+#
+#
+# def requirements():
+#     with open('requirements.txt', 'r') as f:
+#         return [_.strip() for _ in f.readlines() if _.strip()]
+#
+#
+# # ----------------------------------------------------------------------------------------------------------------------
+# # Set up the C++ extensions for compilation
+#
+#
 basedir = 'src'
 
 pyxfiles = [
@@ -54,16 +54,16 @@ pyxfiles = [
     "jpt/distributions/quantile/cdfreg.pyx",
     "jpt/learning/impurity.pyx",
 ]
-
-# ----------------------------------------------------------------------------------------------------------------------
-# We set the CPATH variable because the "include_dir" argument doesn't seem to work properly
-
-
-_numpy_include_dir = numpy.get_include()
-os.environ['CPATH'] = _numpy_include_dir
-print('Setting CPATH environment variable to', _numpy_include_dir)
-
-
+#
+# # ----------------------------------------------------------------------------------------------------------------------
+# # We set the CPATH variable because the "include_dir" argument doesn't seem to work properly
+#
+#
+# _numpy_include_dir = numpy.get_include()
+# os.environ['CPATH'] = _numpy_include_dir
+# print('Setting CPATH environment variable to', _numpy_include_dir)
+#
+#
 # ----------------------------------------------------------------------------------------------------------------------
 # Compile the C++ extensions
 
@@ -76,23 +76,6 @@ compiled = cythonize(
 # ----------------------------------------------------------------------------------------------------------------------
 
 setup(
-    name='pyjpt',
-    packages=[
-        'jpt',
-        'jpt.learning',
-        'jpt.inference',
-        'jpt.base',
-        'jpt.distributions',
-        'jpt.distributions.quantile',
-        'jpt.distributions.univariate'
-    ],
-    package_dir={'': 'src'},
     ext_modules=compiled,
-    zip_safe=False,
     version=__version__,
-    install_requires=requirements(),
-    long_description=__description__,
-    package_data={'jpt': ['.version']},
-    include_package_data=True,
-    extras_require={'mlflow': ['mlflow >= 2.5.0']}
 )
